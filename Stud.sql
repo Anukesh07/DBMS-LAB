440ec4e7ab38b2ad55ae11909639139629829ef8
Anukesh07 <53857067+Anukesh07@users.noreply.github.com>
2020-05-05 22:43:13 +0530

    Update Stud.sql

--- Stud.sql
+++ Stud.sql
@@ -52,3 +52,34 @@ values
 (2,"4-A"),
 (3,"5-A"),
 (4,"6-A");
+
+select distinct sname 
+   from student s,class c,enrolled e,faculty f
+   where s.snum = e.snum and e.cname = c.cname and c.fid = f.fid and f.fname = 'NV' and s.lvl = 'jr';
+select c.cname  
+   from class c
+   where c.room = 'C-207'
+   or c.cname in (select e.cname from enrolled e group by e.cname having count(*) >= 5);
+select distinct sname
+   from student s
+   where s.snum in (select e1.snum
+   from enrolled e1,enrolled e2,class c1,class c2
+   where e1.snum=e2.snum and e1.cname=c1.cname and e2.cname=c2.cname and c1.meets_at='11:15:02');
+select distinct f.fname
+   from faculty f
+   where not exists((select c.room from class c)
+   minus
+   (select c1.room from class c1 where c1.fid=f.fid));
+select distinct fname 
+   from faculty f where 5>(select count(e.snum)
+   from class c,enrolled e where c.cname=e.cname and c.fid=f.fid);
+select distinct sname 
+   from student s
+   where s.snum not in(select e.snum from enrolled e);
+select s.age,s.lvl 
+   from student s
+   group by s.age,s.lvl
+   having s.lvl in(select s1.lvl from student s1 where s1.age=s.age
+                   group by s1.lvl,s1.age
+                   having count(*)>=all(select count(*)
+                   from student s2 where s1.age=s2.age group by s2.lvl,s2.age));
