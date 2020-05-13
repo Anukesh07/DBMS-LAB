28fcec1f673917140e1a78f9a3f315cba4125e5c
Anukesh07 <53857067+Anukesh07@users.noreply.github.com>
2020-05-13 17:30:51 +0530

    Update Supply.sql

--- Supply.sql
+++ Supply.sql
@@ -1,36 +1,36 @@
-use 1bm18cs038;
-create table supplier(
+use cs038;
+create table Suppliers(
 sid int,
 sname varchar(20),
 city varchar(20),
 primary key(sid)
 );
-create table parts(
+create table Parts(
 pid int,
 pname varchar(20),
 color varchar(20),
 primary key(pid)
 );
-create table catalog(
+create table Catalog(
 sid int,
 pid int,
 cost int,
 primary key(sid,pid),
-foreign key(sid) references supplier(sid) on delete cascade,
-foreign key(pid) references parts(pid) on delete cascade
+foreign key(sid) references Suppliers(sid),
+foreign key(pid) references Parts(pid)
 );
-insert into supplier
-values(10001,"acme widget","bangalore"),
-(10002,"johns","kolkata"),
-(10003,"vimal","mumbai"),
-(10004,"reliance","delhi");
-insert into parts
-values(20001,"book","red"),
-(20002,"pen","red"),
-(20003,"pencil","green"),
-(20004,"moblie","green"),
-(20005,"charger","black");
-insert into catalog
+insert into Suppliers
+values(10001,"Acme Widget","Bangalore"),
+(10002,"Johns","Kolkata"),
+(10003,"Vimal","Mumbai"),
+(10004,"Reliance","Delhi");
+insert into Parts
+values(20001,"Book","Red"),
+(20002,"Pen","Red"),
+(20003,"Pencil","Green"),
+(20004,"Moblie","Green"),
+(20005,"Charger","Black");
+insert into Catalog
 values(10001,20001,10),
 (10001,20002,10),
 (10001,20003,30),
@@ -41,11 +41,11 @@ values(10001,20001,10),
 (10003,20003,30),
 (10004,20003,40);
 select distinct sid
-from catalog,parts
-where catalog.pid=parts.pid and parts.color="red" or parts.color="green";
-select catalog.sid
-from supplier,parts,catalog
-where supplier.sid=catalog.sid and catalog.pid=parts.pid and (parts.color="red" or supplier.city="bangalore");
+from Catalog,Parts
+where Catalog.pid=Parts.pid and Parts.color="Red" or Parts.color="Green";
+select Catalog.sid
+from Suppliers,Parts,Catalog
+where Suppliers.sid=Catalog.sid and Catalog.pid=Parts.pid and (Parts.color="Red" or Suppliers.city="Bangalore");
 select c1.sid
 from catalog c1,catalog c2
 where c1.pid=c2.pid and c1.cost>c2.cost;
